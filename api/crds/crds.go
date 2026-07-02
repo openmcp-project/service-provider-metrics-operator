04fac7c417bf7b329c08b0dcbb6f5ce3a5764386
Michael Sprauer <Michael.Sprauer@sap.com>
2026-07-02 12:53:23 +0200

    fix: formatting
    Signed-off-by: Michael Sprauer <Michael.Sprauer@sap.com>

--- api/crds/crds.go
+++ api/crds/crds.go
@@ -3,8 +3,9 @@ package crds
 import (
 	"embed"
 
-	crdutil "github.com/openmcp-project/controller-utils/pkg/crds"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+	crdutil "github.com/openmcp-project/controller-utils/pkg/crds"
 )
 
 //go:embed manifests
